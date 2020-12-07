b90eb59d8faab0c0f28f79a860b5400fe267d2d8
Vladislav.Kirilenok <vladislav.kirilenok@gmail.com>
2020-12-07 18:51:32 +0300

    added states

---
+++ src/constants/states.js
@@ -0,0 +1,69 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2020 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+export default {
+  "US-AL": "Alabama",
+  "US-AK": "Alaska",
+  "US-AZ": "Arizona",
+  "US-AR": "Arkansas",
+  "US-CA": "California",
+  "US-CO": "Colorado",
+  "US-CT": "Connecticut",
+  "US-DE": "Delaware",
+  "US-DC": "District of Columbia	Federal district",
+  "US-FL": "Florida",
+  "US-GA": "Georgia",
+  "US-HI": "Hawaii",
+  "US-ID": "Idaho",
+  "US-IL": "Illinois",
+  "US-IN": "Indiana",
+  "US-IA": "Iowa",
+  "US-KS": "Kansas",
+  "US-KY": "Kentucky",
+  "US-LA": "Louisiana",
+  "US-ME": "Maine",
+  "US-MD": "Maryland",
+  "US-MA": "Massachusetts",
+  "US-MI": "Michigan",
+  "US-MN": "Minnesota",
+  "US-MS": "Mississippi",
+  "US-MO": "Missouri",
+  "US-MT": "Montana",
+  "US-NE": "Nebraska",
+  "US-NV": "Nevada",
+  "US-NH": "New Hampshire",
+  "US-NJ": "New Jersey",
+  "US-NM": "New Mexico",
+  "US-NY": "New York",
+  "US-NC": "North Carolina",
+  "US-ND": "North Dakota",
+  "US-OH": "Ohio",
+  "US-OK": "Oklahoma",
+  "US-OR": "Oregon",
+  "US-PA": "Pennsylvania",
+  "US-RI": "Rhode Island",
+  "US-SC": "South Carolina",
+  "US-SD": "South Dakota",
+  "US-TN": "Tennessee",
+  "US-TX": "Texas",
+  "US-UT": "Utah",
+  "US-VT": "Vermont",
+  "US-VA": "Virginia",
+  "US-WA": "Washington",
+  "US-WV": "West Virginia",
+  "US-WI": "Wisconsin",
+  "US-WY": "Wyoming",
+};
