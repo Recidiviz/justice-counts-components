2875a2fc2c6ec939ded9459d2c95142b4a06559e
Vladislav.Kirilenok <vladislav.kirilenok@gmail.com>
2020-12-29 15:51:20 +0300

    fixed app was not running in IE11

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
+/** @jsxRuntime classic */
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import React from "react";
