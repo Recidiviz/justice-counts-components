ae18cc59cd7c1d0eae3f5bd2a3ae7012254bc30b
Colin Adams <colin@recidiviz.org>
2021-05-28 16:55:10 -0700

    add test

---
+++ src/utils/__tests__/generateMeasurementTypeText.js
@@ -0,0 +1,39 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2021 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// =============================================================================
+import generateMeasurementTypeText from "../generateMeasurementTypeText";
+
+describe("generateMeasurementTypeText.js", () => {
+  const averageText = generateMeasurementTypeText("AVERAGE");
+  it("should handle average", () => {
+    expect(averageText).toBe("Average");
+  });
+
+  const instantText = generateMeasurementTypeText("INSTANT");
+  it("should handle instant", () => {
+    expect(instantText).toBe("Point in Time");
+  });
+
+  const deltaText = generateMeasurementTypeText("DELTA");
+  it("should produce nothing for delta", () => {
+    expect(deltaText).toBe("");
+  });
+
+  const undefinedText = generateMeasurementTypeText(undefined);
+  it("should produce nothing for undefined", () => {
+    expect(undefinedText).toBe("");
+  });
+});
